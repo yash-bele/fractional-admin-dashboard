16a9e65389ff243c7fc57a63e26efd5949e47210
Yash Bele <109889223+yash-bele@users.noreply.github.com>
2023-07-01 23:04:28 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,22 +5,21 @@ const Login = () => {
   const handleSubmit = () => navigate("/home");
 
   return (
-    <section className="absolute w-full h-full overflow-hidden">
-      <div class="flex flex-col items-center justify-center px-6 h-screen lg:py-0">
+      <div className="absolute w-full h-full -mt-10 flex flex-col items-center justify-center px-6 lg:py-0">
         <section className="w-fit text-lg text-center text-white mb-5 -mt-10 font-serif">
           <h1>FRACTIONAL ASSET</h1>
           <p className="text-xs">Admin Dashboard</p>
         </section>
-        <div class="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-semibold leading-tight tracking-tight md:text-2xl text-white">
+        <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-semibold leading-tight tracking-tight md:text-2xl text-white">
               Login to your account
             </h1>
             <form>
               <div className="mb-4 md:mb-6">
                 <label
                   for="email"
-                  class="block mb-2 text-sm font-medium text-white"
+                  className="block mb-2 text-sm font-medium text-white"
                 >
                   Your email
                 </label>
@@ -28,7 +27,7 @@ const Login = () => {
                   type="email"
                   name="email"
                   id="email"
-                  class="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                  className="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                   placeholder="name@company.com"
                   required=""
                 />
@@ -36,7 +35,7 @@ const Login = () => {
               <div className="mb-8 md:mb-10">
                 <label
                   for="password"
-                  class="block mb-2 text-sm font-medium text-white"
+                  className="block mb-2 text-sm font-medium text-white"
                 >
                   Password
                 </label>
@@ -45,14 +44,14 @@ const Login = () => {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  class="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                  className="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                   required=""
                 />
               </div>
               <button
                 onClick={handleSubmit}
                 type="submit"
-                class="font-serif w-full text-white text-lg focus:ring-4 focus:outline-nonefont-medium rounded-lg px-5 py-2 text-center duration-300 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
+                className="font-serif w-full text-white text-lg focus:ring-4 focus:outline-nonefont-medium rounded-lg px-5 py-2 text-center duration-300 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
               >
                 Login
               </button>
@@ -60,7 +59,6 @@ const Login = () => {
           </div>
         </div>
       </div>
-    </section>
   );
 };
 
