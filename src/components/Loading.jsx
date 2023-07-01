88f6be8283389c6a1de348c944f5cfb26f64a041
Yash Bele <109889223+yash-bele@users.noreply.github.com>
2023-07-01 20:38:53 +0530

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import { ImSpinner9 } from "react-icons/im";
 
 const Loading = () => {
   return (
-    <main className="absolute w-full h-full grid place-items-center">
+    <main className="absolute w-full h-full overflow-y-scroll grid place-items-center">
       <ImSpinner9 className="text-gray-800 text-9xl animate-spin -mt-10" />
     </main>
   );
